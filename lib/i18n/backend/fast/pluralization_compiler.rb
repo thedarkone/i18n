f1980b2db68f6578ea7709660baaae8aa018adac
thedarkone <thedarkone2@gmail.com>
2009-03-17 03:19:06 +0100

    Rewrote pluralization compiler.

--- lib/i18n/backend/fast/pluralization_compiler.rb
+++ lib/i18n/backend/fast/pluralization_compiler.rb
@@ -3,6 +3,8 @@ module I18n
     class Fast < Simple
       module PluralizationCompiler
         extend self
+        
+        TOKENIZER = /(\\\\\{\{[^\}]+\}\}|\{\{[^\}]+\}\})/
 
         def compile_if_an_interpolation(string)
           if interpolated_str?(string)
@@ -17,26 +19,37 @@ module I18n
         end
 
         def interpolated_str?(str)
-          str.kind_of?(String) && str.scan(Simple::MATCH).find{|escape_chars, interpolation| !escape_chars && interpolation}
+          str.kind_of?(String) && str =~ Simple::MATCH
         end
 
         protected
+        # tokenize("foo {{bar}} baz \\\\{{buz}}") # => ["foo ", "{{bar}}", " baz ", "\\\\{{buz}}"]
+        def tokenize(str)
+          str.split(TOKENIZER)
+        end
+
         def compiled_interpolation_body(str)
-          str.gsub(Simple::MATCH) do
-            escaped, pattern, key = $1, $2, $3.to_sym
+          tokenize(str).map do |token|
+            (matchdata = token.match(Simple::MATCH)) ? handle_interpolation_token(token, matchdata) : escape_plain_str(token)
+          end.join
+        end
 
-            if escaped
-              pattern
-            else
-              eskey = escape_key_sym(key)
-              if Simple::INTERPOLATION_RESERVED_KEYS.include?(key)
-                "\#{raise(ReservedInterpolationKey.new(#{eskey}, self))}"
-              else
-                "\#{values[#{eskey}] || (values.has_key?(#{eskey}) && values[#{eskey}].to_s) || raise(MissingInterpolationArgument.new(#{eskey}, self))}"
-              end
-            end
+        def handle_interpolation_token(interpolation, matchdata)
+          escaped, pattern, key = matchdata.values_at(1, 2, 3)
+          escaped ? pattern : compile_interpolation_token(key.to_sym)
+        end
 
+        def compile_interpolation_token(key)
+          eskey = escape_key_sym(key)
+          if Simple::INTERPOLATION_RESERVED_KEYS.include?(key)
+            "\#{raise(ReservedInterpolationKey.new(#{eskey}, self))}"
+          else
+            "\#{values[#{eskey}] || (values.has_key?(#{eskey}) && values[#{eskey}].to_s) || raise(MissingInterpolationArgument.new(#{eskey}, self))}"
           end
+        end
+
+        def escape_plain_str(str)
+          str.gsub(/"|\\|#/) {|x| "\\#{x}"}
         end
 
         def escape_key_sym(key)
