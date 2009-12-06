b3e8b934ab63849834de7e88d152291ecc58a952
thedarkone <thedarkone2@gmail.com>
2009-12-06 17:06:16 +0100

    Add InterpolationCompiler extension.

--- lib/i18n/backend.rb
+++ lib/i18n/backend.rb
@@ -1,14 +1,15 @@
 module I18n
   module Backend
-    autoload :ActiveRecord,  'i18n/backend/active_record'
-    autoload :Base,          'i18n/backend/base'
-    autoload :Cache,         'i18n/backend/cache'
-    autoload :Chain,         'i18n/backend/chain'
-    autoload :Fallbacks,     'i18n/backend/fallbacks'
-    autoload :Gettext,       'i18n/backend/gettext'
-    autoload :Helpers,       'i18n/backend/helpers'
-    autoload :Metadata,      'i18n/backend/metadata'
-    autoload :Pluralization, 'i18n/backend/pluralization'
-    autoload :Simple,        'i18n/backend/simple'
+    autoload :ActiveRecord,          'i18n/backend/active_record'
+    autoload :Base,                  'i18n/backend/base'
+    autoload :Cache,                 'i18n/backend/cache'
+    autoload :Chain,                 'i18n/backend/chain'
+    autoload :Fallbacks,             'i18n/backend/fallbacks'
+    autoload :Gettext,               'i18n/backend/gettext'
+    autoload :Helpers,               'i18n/backend/helpers'
+    autoload :InterpolationCompiler, 'i18n/backend/interpolation_compiler'
+    autoload :Metadata,              'i18n/backend/metadata'
+    autoload :Pluralization,         'i18n/backend/pluralization'
+    autoload :Simple,                'i18n/backend/simple'
   end
 end
