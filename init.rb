853fe70ca73a71a3e6ee1f3f19b2c466e617b9b4
thedarkone <thedarkone2@gmail.com>
2010-03-22 19:34:00 +0100

    Explicitly remove the plugin from the load path.

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
-# only require the LazyReloading module
-require File.dirname(__FILE__) + '/lib/i18n/backend/lazy_reloading'
+require File.dirname(__FILE__) + '/lib/i18n/backend/lazy_reloading' # only require the LazyReloading module
+$:.delete(File.expand_path(File.dirname(__FILE__) + '/lib')) # get rid of myself from the load path
